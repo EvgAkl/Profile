89ea45ed108d805f9c52a3fa3c5cc381788c44e5
Zhenya <Zhenya@DESKTOP-HSBK723>
2018-06-16 22:49:29 +0300

    (+)  "Model.Database" folder, "DatabaseModel" file "Group" class in "DatabaseModel"

---
+++ Profile/Models/Database/DatabaseModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Profile.Models.Database
+{
+    public enum GroupName
+    {
+        Racers = 1,
+        Bikers,
+        Jokers,
+        Musicians
+    }
+
+    public enum Rank
+    {
+        Tiny = 1,
+        Small,
+        Middle,
+        Large,
+        Hudge,
+        Impossible
+    }
+
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public GroupName Name { get; set; }
+        public int CountMembers { get; set; }
+        public Rank Rank { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int Karma { get; set; }
+        public HttpPostedFileBase Avatar { get; set; } 
+    } // end "Group" class
+
+} // end namespace
