c3fe84baec1d0e34904fa003a3bd0d2bad56ed5a
Zhenya <Zhenya@DESKTOP-HSBK723>
2018-06-17 14:41:27 +0300

    (refactor) transport database data from "Index" method "Admin" controller to "Index" view

--- Profile/Controllers/AdminController.cs
+++ Profile/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Profile.Models.Database;
 
 namespace Profile.Controllers
 {
@@ -10,7 +11,7 @@ namespace Profile.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(DatabaseWorker.GetGroupList());
         }
 
     } // end controller
