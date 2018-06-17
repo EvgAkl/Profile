9e6a7c24e74073391ff6bc3c4cf4f07c0e545672
Zhenya <Zhenya@DESKTOP-HSBK723>
2018-06-17 13:02:02 +0300

    (+) "SetInitializer" database in "Global.asax" file

--- Profile/Global.asax.cs
+++ Profile/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Data.Entity;
+using Profile.Models.Database.Context;
 
 namespace Profile
 {
@@ -13,6 +15,7 @@ namespace Profile
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Database.SetInitializer(new CreateDatabaseIfNotExists<EFContext>());
         }
-    }
-}
+    } // end class
+} // end namespace
