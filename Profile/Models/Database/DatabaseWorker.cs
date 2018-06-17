a89700f4b3cf1a129497d15e984530474729db89
Zhenya <Zhenya@DESKTOP-HSBK723>
2018-06-17 14:37:33 +0300

    (+) "DatabaseWorker" static class and "GetGroupList" method in it

---
+++ Profile/Models/Database/DatabaseWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Profile.Models.Database.Context;
+
+namespace Profile.Models.Database
+{
+    public static class DatabaseWorker
+    {
+        public static List<Group> GetGroupList()
+        {
+            List<Group> dataList = new List<Group>();
+            using (EFContext database = new EFContext())
+            {
+                dataList = database.Groups.Select(s => s).ToList();
+            }
+            return dataList;
+        } // end GetGroupList()
+
+    } // end class
+} // end namespace
