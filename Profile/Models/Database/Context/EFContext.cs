795ba6d550fedab6fade5c0acf3876a0831681be
Zhenya <Zhenya@DESKTOP-HSBK723>
2018-06-17 12:52:58 +0300

    (+) "Model.Databse.Context" folder and "EFContext" class in it (EntityFramework context ckass)

---
+++ Profile/Models/Database/Context/EFContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Profile.Models.Database.Context
+{
+    public class EFContext : DbContext
+    {
+        public DbSet<Group> Groups { get; set; }
+    } // end class
+} // end namespace
