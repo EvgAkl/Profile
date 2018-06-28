646b548aecf8e7fbb4acd54ab64ce536f95dfec1
Zhenya <zhenya-akulich@mail.ru>
2018-06-28 11:32:15 +0300

    (+) "NinjectDependencyResolver" class in "Infrastructure" folder; Set "NinjectDependencyResolver" as resolver in "NinjectWebCommon" class

--- Profile/App_Start/NinjectWebCommon.cs
+++ Profile/App_Start/NinjectWebCommon.cs
@@ -5,6 +5,7 @@ namespace Profile.App_Start
 {
     using System;
     using System.Web;
+    using System.Web.Mvc;
 
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
@@ -61,6 +62,7 @@ namespace Profile.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            DependencyResolver.SetResolver(new Infrastructure.NinjectDependencyResolver(kernel));
         }        
-    }
-}
+    } // end class
+} // end namespace
---
+++ Profile/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using Ninject;
+using Profile.Models.Database;
+using Profile.Models.Database.AbstractWorkers;
+
+
+namespace Profile.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBinding();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBinding()
+        {
+
+        } // end AddBinding()
+
+    } // end class
+} // end namespace
