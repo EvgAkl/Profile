f9e135e339c41eedfa0444bac6ae2d31bb4df89b
Zhenya <zhenya-akulich@mail.ru>
2018-07-02 14:05:48 +0300

    (refactor) "Can_Show_Groups_List" test method => this unit test - OK

--- Profile.Tests/AdminGroupControllerTest.cs
+++ Profile.Tests/AdminGroupControllerTest.cs
@@ -14,46 +14,71 @@ namespace Profile.Tests
     [TestClass]
     public class AdminGroupControllerTest
     {
-        [TestMethod]
-        public void Can_Show_Groups_List()
+        private List<Group> testList = new List<Group>()
         {
-            // Arrange
-            Mock<IWorkerAdminGroup> mock = new Mock<IWorkerAdminGroup>();
-            mock.Setup(m => m.GetGroupList()).Returns(new List<Group>
-            {
-                new Group
+            new Group
                 {
                     GroupId = 1,
                     Name = "Group 1",
-                    CountMembers = 1,
-                    Karma = 1,
+                    CountMembers = 11,
+                    Karma = 111,
                     Rank = Rank.Tiny,
-                    ImageFileSystemPath = "~/Content/UserImage/Image1.png",
+                    ImageFileSystemPath = "D:/Profile/Content/UserImage/Image1.png",
                     ImageProgectLinkPath = "~/Content/UserImage/Image1.png"
                 },
                 new Group
                 {
                     GroupId = 2,
                     Name = "Group 2",
-                    CountMembers = 222,
-                    Karma = 2,
+                    CountMembers = 22,
+                    Karma = 222,
                     Rank = Rank.Middle,
-                    ImageFileSystemPath = "~/Content/UserImage/Image2.png",
+                    ImageFileSystemPath = "D:/Profile/Content/UserImage/Image2.png",
                     ImageProgectLinkPath = "~/Content/UserImage/Image2.png"
                 }
-            });
+        };
+
+        private Group testGroup = new Group
+        {
+            GroupId = 1,
+            Name = "Group 1",
+            CountMembers = 11,
+            Karma = 111,
+            Rank = Rank.Tiny,
+            ImageFileSystemPath = "D:/Profile/Content/UserImage/Image1.png",
+            ImageProgectLinkPath = "~/Content/UserImage/Image1.png"
+        };
+
+        [TestMethod]
+        public void Can_Show_Groups_List()
+        {
+            // Arrange
+            Mock<IWorkerAdminGroup> mock = new Mock<IWorkerAdminGroup>();
+            mock.Setup(m => m.GetGroupList()).Returns(testList);
             AdminGroupController controller = new AdminGroupController(mock.Object);
             // Act
             List<Group> result = ((IEnumerable<Group>)controller.Index().ViewData.Model).ToList();
             // Assert
+            Assert.AreEqual(result.Count, 2);
+            Assert.AreEqual(result[0].GroupId, 1);
+            Assert.AreEqual(result[0].Name, "Group 1");
+            Assert.AreEqual(result[0].CountMembers, 11);
+            Assert.AreEqual(result[0].Karma, 111);
+            Assert.AreEqual(result[0].Rank, Rank.Tiny);
+            Assert.AreEqual(result[0].ImageFileSystemPath, "D:/Profile/Content/UserImage/Image1.png");
+            Assert.AreEqual(result[0].ImageProgectLinkPath, "~/Content/UserImage/Image1.png");
+            Assert.AreEqual(result[1].GroupId, 2);
+            Assert.AreEqual(result[1].Name, "Group 2");
+            Assert.AreEqual(result[1].CountMembers, 22);
+            Assert.AreEqual(result[1].Karma, 222);
+            Assert.AreEqual(result[1].Rank, Rank.Middle);
+            Assert.AreEqual(result[1].ImageFileSystemPath, "D:/Profile/Content/UserImage/Image2.png");
+            Assert.AreEqual(result[1].ImageProgectLinkPath, "~/Content/UserImage/Image2.png");
+        } // end Can_Show_Groups_List()
 
 
 
 
-
-
-
-        }
 
     } // end class
 } // end namespace
